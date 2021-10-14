0ec54c7453238fe6dd31fa2be32d12c3130427d0
Yueqian Liu <yueqianliu@outlook.com>
2021-10-14 20:25:08 +0800

    script for rm files caused by switching px4 ver

---
+++ clean_px4_future.sh
@@ -0,0 +1,4 @@
+rm -rf PX4-Autopilot/Tools/simulation-ignition/
+rm -rf PX4-Autopilot/src/lib/events/
+echo "rm -rf PX4-Autopilot/Tools/simulation-ignition/"
+echo "rm -rf PX4-Autopilot/src/lib/events/"
---
+++ clean_px4_legacy.sh
@@ -0,0 +1,6 @@
+rm -rf PX4-Autopilot/boards/atlflight/
+rm -rf PX4-Autopilot/platforms/qurt/
+rm -rf PX4-Autopilot/src/lib/ecl/
+echo "rm -rf PX4-Autopilot/boards/atlflight/"
+echo "rm -rf PX4-Autopilot/platforms/qurt/"
+echo "rm -rf PX4-Autopilot/src/lib/ecl/"
