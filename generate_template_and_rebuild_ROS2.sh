65e2582c1b1eb6f6cd6d3b4f51cf9db179e8ee14
Yueqian Liu <yueqianliu@outlook.com>
2021-10-14 22:13:51 +0800

    script for generating rtps topics

---
+++ generate_template_and_rebuild_ROS2.sh
@@ -0,0 +1,2 @@
+python3 ./PX4-Autopilot/msg/tools/uorb_to_ros_urtps_topics.py -i ./PX4-Autopilot/msg/tools/urtps_bridge_topics.yaml -o ./ros2_workspace/src/px4_ros_com/templates/urtps_bridge_topics.yaml
+./ros2_workspace/src/px4_ros_com/scripts/build_ros2_workspace.bash
