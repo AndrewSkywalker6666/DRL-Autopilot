fb06b5f6a70567fed5ca10fbcf2e053956d4b062
Yueqian Liu <yueqianliu@outlook.com>
2021-10-30 19:39:32 +0800

    add ros2 uorb sync script

--- generate_template_and_rebuild_ROS2.sh
+++ generate_template_and_rebuild_ROS2.sh
@@ -1,2 +1,3 @@
 python3 ./PX4-Autopilot/msg/tools/uorb_to_ros_urtps_topics.py -i ./PX4-Autopilot/msg/tools/urtps_bridge_topics.yaml -o ./ros2_workspace/src/px4_ros_com/templates/urtps_bridge_topics.yaml
+python3 ./PX4-Autopilot/msg/tools/uorb_to_ros_msgs.py ./PX4-Autopilot/msg/ ./ros2_workspace/src/px4_msgs/msg/
 ./ros2_workspace/src/px4_ros_com/scripts/build_ros2_workspace.bash
