a984567fc85e6e6527b43acab3ab9ede64ae5f0a
Yueqian Liu <yueqianliu@outlook.com>
2021-11-21 10:40:34 +0800

    include more models and custom plugin path

--- sim_gazebo_ros2.sh
+++ sim_gazebo_ros2.sh
@@ -6,9 +6,10 @@ DONT_RUN=1 make px4_sitl_default gazebo
 cd ..
 
 # export plugin and library path
-export GAZEBO_PLUGIN_PATH=$GAZEBO_PLUGIN_PATH:./PX4-Autopilot/build/px4_sitl_default/build_gazebo
+export GAZEBO_PLUGIN_PATH=$GAZEBO_PLUGIN_PATH:./PX4-Autopilot/build/px4_sitl_default/build_gazebo:./ros2_workspace/install/custom_gazebo_plugins/lib
 export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:./PX4-Autopilot/Tools/sitl_gazebo/models
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./PX4-Autopilot/build/px4_sitl_default/build_gazebo
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./PX4-Autopilot/build/px4_sitl_default/build_gazebo:./ros2_workspace/install/custom_gazebo_plugins/lib
 
 # launch world for training, check if .gazebo folder contains files with the same name
-gazebo ./PX4-Autopilot/Tools/sitl_gazebo/worlds/omni_hex_ros2.world -s libgazebo_ros_init.so -s libgazebo_ros_factory.so -s libgazebo_ros_force_system.so --verbose
+# available: omni_hex_ros2, iris_ros2
+gazebo ./PX4-Autopilot/Tools/sitl_gazebo/worlds/iris_ros2.world -s libgazebo_ros_init.so -s libgazebo_ros_factory.so -s libgazebo_ros_force_system.so --verbose
